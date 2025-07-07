fdbe7a3da1075b30b535441f8c083bad6d9e69ba
harshidml <harsh@idesignmylife.net>
2025-07-07 11:17:59 +0000

    Final Commit

--- enrollments/enrollments/doctype/courses/courses.py
+++ enrollments/enrollments/doctype/courses/courses.py
@@ -1,9 +1,20 @@
-# Copyright (c) 2025, Harsh and contributors
+# Copyright (c) 2025, Harsh Makwana and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
+class Enrollment(Document):
+    def validate(self):
+        course_doc = frappe.get_doc("Courses", self.course)
 
-class Courses(Document):
-	pass
+        if course_doc.available_seats <= 0:
+            frappe.throw(f"No seats available for {course_doc.name}. {course_doc.available_seats} seats left.")
+
+        self.course_fees = course_doc.price
+        self.total_fees = course_doc.price
+
+    def after_insert(self):
+        course_doc = frappe.get_doc("Courses", self.course)
+        course_doc.available_seats -= 1
+        course_doc.save()
